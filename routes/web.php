3e486b03369a21e2a970161f4caecb190ad84b8d
d1k4s <dikaest09@gmail.com>
2023-02-13 10:13:23 +0700

    acara 5

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\ProvisionServer;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'ngak ngik ngok gomenasai 🐷🐷🐷🐷';
 });
+Route::get('/user', [UserController::class, "index"]);
+Route::get('/server', [ProvisionServer::class, "__invoke"]);
+Route::get('/photo', [PhotoController::class, "create"]);
